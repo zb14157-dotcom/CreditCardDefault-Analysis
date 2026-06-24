a90c3af197270dfd7673a3a316a939ee52eb4c19
Zach Baker <zb14157@gmail.com>
2026-06-23 22:01:23 -0500

    Add query for overall default rate baseline analysis

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -1,1 +1,6 @@
-
+"Added Query 1 - Overall default rate baseline"
+-- Overall Default Rate
+Select Count(*) AS TotalCustomer,
+SUM(`default.payment.next.month`) AS TotalDefaults,
+ROUND(AVG(`default.payment.next.month`) * 100,2) AS DefaultRate
+FROM schema.uci_credit_card; 
